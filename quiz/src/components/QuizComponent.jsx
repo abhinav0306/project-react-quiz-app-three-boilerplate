5a4f5a745c967cd4d65dc76a93a3a4b9f6d4c90f
abhinav0306 <abhinavs0603@gmail.com>
2024-01-28 19:23:36 +0530

    quizz app

--- quiz/src/components/QuizComponent.jsx
+++ quiz/src/components/QuizComponent.jsx
@@ -1,54 +1,84 @@
-import React from 'react'
-import questions from "../resources/quizQuestion.json"
-export default class QuizComponent extends React.Component {
-    constructor() {
-        super();
-        this.state={
-            currentIndex:0
+import { useState } from 'react';
+import questions from "../resources/quizQuestion.json";
+import { Link } from "react-router-dom";
+
+const QuizComponent = () => {
+    const [current, setCurrent] = useState(0);
+    const [attempted, setAttempted] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    const [wrong, setWrong] = useState(0);
+
+    const handlePrev = () => {
+        setCurrent(prevIndex => Math.max(0, prevIndex - 1));
+    };
+
+    const handleNext = () => {
+        setCurrent(prevIndex => Math.min(14, prevIndex + 1));
+    };
+
+    const handleQuit = () => {
+        alert("Are you sure you want to exit?");
+        setCurrent(0);
+        setAttempted(0);
+        setCorrect(0);
+        setWrong(0);
+        
+       
+    };
+    
+
+    const handleAnswer = (selectedOption) => {
+        const currentQuestion = questions[current];
+        let attemptedCount = attempted;
+        let correctCount = correct;
+        let wrongCount = wrong;
+        
+        if (selectedOption === currentQuestion.answer) {
+            alert("Correct answer!");
+            correctCount += 1;
+        } else {
+            alert("Wrong answer!");
+            wrongCount += 1;
         }
+        
+        attemptedCount += 1;
+    
+        setAttempted(attemptedCount);
+        setCorrect(correctCount);
+        setWrong(wrongCount);
+    
+        
+        localStorage.setItem('quizScores', JSON.stringify({ attempted: attemptedCount, correct: correctCount, wrong: wrongCount }));
+        
+        handleNext();
+    };
+    
 
-    }
-    handlePrev=()=>{
-        this.setState(prev=>({
-            currentIndex: Math.max(0,prev.currentIndex-1)
-        }))
-    }
-    handleNext=()=>{
-        this.setState(prev=>({
-            currentIndex: Math.min(14,prev.currentIndex+1)
-        }))
-    }
-    handleQuit=()=>{
-        alert("Are you sure to exit?")
-        this.setState(prev=>({
-            currentIndex:0
-        }))
-    }
-    render() { 
-        let {currentIndex}=this.state
-        let{question,optionA,optionB,optionC,optionD}=questions[currentIndex]
+    const { question, optionA, optionB, optionC, optionD } = questions[current];
 
-        return ( 
-            <>
+    return (
+        <>
             <div className="body2">
-            <div className="containerquiz">
-                <h2>Question</h2>
-                <p className='Qnum'>{currentIndex+1} of 15</p>
-                <h4>{question}</h4>
-                <div className="options">
-                    <button>{optionA}</button>
-                    <button>{optionB}</button>
-                    <button>{optionC}</button>
-                    <button>{optionD}</button>
-                </div>
-                <div className="navButtons">
-                    <button id='prev' onClick={this.handlePrev}>Previous</button>
-                    <button id='next' onClick={this.handleNext}>Next</button>
-                    <button id='quit'onClick={this.handleQuit}>Quit</button>
+                <div className="containerquiz">
+                    <h2>Question</h2>
+                    <p className='Qnum'>{current + 1} of 15</p>
+                    <h4>{question}</h4>
+                    <div className="options">
+                        <button onClick={() => handleAnswer(optionA)}>{optionA}</button>
+                        <button onClick={() => handleAnswer(optionB)}>{optionB}</button>
+                        <button onClick={() => handleAnswer(optionC)}>{optionC}</button>
+                        <button onClick={() => handleAnswer(optionD)}>{optionD}</button>
+                    </div>
+                    <div className="navButtons">
+                        <button id='prev' onClick={handlePrev}>Previous</button>
+                        <button id='next' onClick={handleNext}>Next</button>
+                        <button id='quit' onClick={handleQuit}>Quit</button>
+                        <Link to={"/result"}><button id='finish'>Finish</button></Link>
+                    </div>
                 </div>
             </div>
-            </div>
-            </>
-         );
-    }
-}
+        </>
+    );
+};
+
+export default QuizComponent;
--- quiz/src/components/ResultComponent.jsx
+++ quiz/src/components/ResultComponent.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
-export default class ResultComponent extends React.Component {
-    constructor() {
-        super();
+import  { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-    }
-    render() { 
-        return ( 
-            <>
+const ResultComponent = () => {
+    const [attempted, setAttempted] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    const [wrong, setWrong] = useState(0);
+
+    useEffect(() => {
+        const quizScores = JSON.parse(localStorage.getItem('quizScores'));
+            setAttempted(quizScores.attempted);
+            setCorrect(quizScores.correct);
+            setWrong(quizScores.wrong);
+    }, []);
+
+    const percentage = () => {
+        return parseInt((correct / 15) * 100);
+    };
+
+    return (
+        <>
             <div className="body3">
-            <h1>Result</h1>
-            <div className="containerResult">
-                <h3>You need more practice!</h3>
-                <h1 className='score'>Your score is 20%</h1>
-                <div className="results">
-                <p>Total number of questions: 15 </p>
-                <p>Number of attempted questions: 9 </p>
-                <p>Number of correct answers: 3</p>
-                <p>Number of wrong answers: 6</p>
+                <h1>Result</h1>
+                <div className="containerResult">
+                    <h3> {percentage() >50 ?"Congratulations You passed!" : "Practice more!"} </h3>
+                    <h1 className='score'>Your score is {percentage()}%</h1>
+                    <div className="results">
+                        <p>Total number of questions: 15 </p>
+                        <p>Number of attempted questions: {attempted} </p>
+                        <p>Number of correct answers: {correct}</p>
+                        <p>Number of wrong answers: {wrong}</p>
+                    </div>
                 </div>
-                
+                <div className="btns">
+                    <Link to={"/quiz"}>
+                        <button className='btn' id='play'>Play Again</button>
+                    </Link>
+                    <Link to={"/"}>
+                        <button className='btn' id='back'>Back to home</button>
+                    </Link>
+                </div>
             </div>
-            <div className="btns">
-            <button className='btn' id='play'>Play Again</button>
-            <button className='btn' id='back'>Back to home</button>
-            </div>
-            </div>
-            </>
-         );
-    }
-}
+        </>
+    );
+};
+
+export default ResultComponent;
